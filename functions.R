1d8bcc5012ea7079a09bdb22f32fd5c782095bf4
Bjoern Siepe <bjoernsiepe@gmail.com>
2024-10-30 15:02:45 +0000

    add robustness replication fns

--- functions.R
+++ functions.R
@@ -93,3 +93,123 @@ compare_point_ests <- function(original_folder,
 }
 
 
+# Publication Bias Analyses -----------------------------------------------
+estimate_robma <- function(prefix,
+                           dataset_id = 1,
+                           parallel = TRUE,
+                           ...) {
+  # read in data
+  df_data <- readRDS(here(paste0(
+    "data/data_", prefix, "_multiple_final.RData"
+  )))
+  
+  est_pub_bias <- RoBMA::RoBMA(
+    y = df_data[[dataset_id]]$Effect_Size,
+    se = df_data[[dataset_id]]$Effect_Size_se,
+    study_ids = df_data[[dataset_id]]$id_site,
+    parallel = parallel,
+    seed = 35037,
+    ...
+  )
+  return(est_pub_bias)
+}
+
+
+
+# Model Weights Analyses --------------------------------------------------
+load_models <- function(prefix, replication = TRUE) {
+  file_names <- c(
+    "environment_m_rep.rds",
+    "task_m_rep.rds",
+    "task_environment_language_age_m_rep.rds",
+    "task_language_age_m_rep.rds",
+    "environment_language_age_m_rep.rds",
+    "age_language_m_rep.rds",
+    "age_m_rep.rds",
+    "language_m_rep.rds",
+    "intercept_m_rep.rds"
+  )
+  # if original models instead of our replications should be used
+  if (isFALSE(replication)) {
+    file_names <- gsub("_rep", "", file_names)
+  }
+  final_names <- c(
+    "environment",
+    "task",
+    "task_environment_language_age",
+    "task_language_age",
+    "environment_language_age",
+    "age_language",
+    "age",
+    "language",
+    "intercept"
+  )
+  
+  models <- list()
+  for (i in seq_along(file_names)) {
+    if (isTRUE(replication)) {
+      model <- readRDS(paste0("models/replication/", prefix, "_", file_names[i]))
+    } else{
+      model <- readRDS(paste0("models/", prefix, "_", file_names[i]))
+    }
+    models[[final_names[i]]] <- model
+  }
+  return(models)
+}
+
+# safely compute weights
+compute_weights_safe <- function(models, method) {
+  tryCatch({
+    # unfortunately had to hardcode this, other solutions did not work
+    # as intended
+    weights <- brms::model_weights(
+      models[[1]],
+      models[[2]],
+      models[[3]],
+      models[[4]],
+      models[[5]],
+      models[[6]],
+      models[[7]],
+      models[[8]],
+      models[[9]],
+      weights = method
+    )
+    names(weights) <- names(models)
+    return(weights)
+  }, error = function(e) {
+    message(paste("Error in model_weights with method", method, ": ", e$message))
+    return(NULL)
+  })
+}
+
+# compute all model weights for a given prefix
+compute_model_weights <- function(prefix, pmp = FALSE) {
+  models <- load_models(prefix)
+  
+  weights <- list(
+    loo = compute_weights_safe(models, "loo"),
+    stacking = compute_weights_safe(models, "stacking"),
+    waic = compute_weights_safe(models, "waic"),
+    if (isTRUE(pmp)) {
+      pmp = tryCatch({
+        post_prob(
+          models$environment,
+          models$task,
+          models$task_environment_language_age,
+          models$task_language_age,
+          models$environment_language_age ,
+          models$age_language,
+          models$age,
+          models$language,
+          models$intercept
+        )
+      }, error = function(e) {
+        message(paste("Error in post_prob for prefix", prefix, ": ", e$message))
+        return(NULL)
+      })
+    }
+  )
+  
+  rm(models) # Remove models from memory after calculation
+  return(weights)
+}
